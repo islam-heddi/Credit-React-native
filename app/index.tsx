859095f536b79392ee4d23f19f43e3294d8b3b6c
Islam Heddi <117187090+islam-heddi@users.noreply.github.com>
2026-08-14 21:03:26 +0000

    add coin png

--- app/index.tsx
+++ app/index.tsx
@@ -1,104 +1,23 @@
-import { DataWithPagination } from "@/component/DataWithPagination";
-import type { IMoney } from "@/types/Money";
-import { Link } from "expo-router";
+import { useRouter } from "expo-router";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
-
+import { Button, Image, Text, View } from "react-native";
 export default function Index() {
-  const data: IMoney[] = [
-    {
-    amount: 230,
-    fromPerson: "ahmed"
-  },{
-    amount: 230,
-    fromPerson: "ahmed"
-  },{
-    amount: 230,
-    fromPerson: "ahmed"
-  },{
-    amount: 230,
-    fromPerson: "ahmed"
-  },{
-    amount: 230,
-    fromPerson: "ahmed"
-  },{
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ahmed"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-  {
-    amount: 9000230,
-    fromPerson: "ashref"
-  },
-];
+  const route = useRouter()
   return (
-    <ScrollView>
-      <View>
-        <Text>You see your refunds below</Text>
-        <Link href={"/about"}>about</Link>
-        <Link href={"/register"}>register</Link>
-        <View style={{
-          margin: 20
-        }}>
-        <DataWithPagination data={data} />
-        </View>
+    <View  style={{ 
+      flex: 1,
+      justifyContent: "flex-start",
+      backgroundColor: '#fff',
+      gap:10,
+      padding: 20,
+    }}>
+        <Text style={{fontSize: 30, alignSelf: "center"}}>My Money</Text>
+        <Image source={require("../assets/images/coin.png")} style={{
+          width: 300,
+          height: 300,
+           alignSelf: "center"
+        }} />
+        <Button title="Start" onPress={() => route.push("/register")} />      
       </View>
-    </ScrollView>
   );
 }
